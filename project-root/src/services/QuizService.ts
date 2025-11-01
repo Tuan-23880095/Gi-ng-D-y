e9238f1e3f348f925a3ab8debc1f16cc6e5b3df9
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2025-11-01 20:03:27 +0700

    Update QuizService.ts

--- project-root/src/services/QuizService.ts
+++ project-root/src/services/QuizService.ts
@@ -1,1 +1,14 @@
+import { ContentRepository } from '../data/ContentRepository';
+import { ChoiceQuestion } from '../models/ChoiceQuestion';
+import { TrueFalseQuestion } from '../models/TrueFalseQuestion';
+import { ShortAnswerQuestion } from '../models/ShortAnswerQuestion';
+import { Quiz } from '../models/Quiz';
 
+
+export class QuizService {
+  constructor(private repo: ContentRepository){}
+  async loadQuiz(id: string): Promise<Quiz>{
+    const raw = await this.repo.getQuizById(id);
+    return raw;
+  }
+}
